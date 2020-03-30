7f03e4413d1e42e9265d790cb64c41f34dbea5ab
Akshay Sood <sood.iitd@gmail.com>
2020-03-30 16:14:33 -0500

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     packages=find_packages(),
     python_requires=">= 3.5",
     url="https://github.com/Craven-Biostat-Lab/mihifepe",
-    version="0.2.1",
+    version="0.2.2",
     zip_safe=True,
 )
